e99af314df8dfe484ac5d6e8996419535995805b
Andy Bryson <abryson@redhat.com>
2024-12-03 13:29:18 -0500

    tweaks to run-local.sh, api gateway seems to be working

--- stop-local.sh
+++ stop-local.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo stopping create-loan / saga application
-kill -9 $(cat create-loan.pid) && rm create-loan.pid
+kill -9 $(cat create-loan.pid) && rm -f create-loan.pid
 
 echo stopping loan service
-kill -9 $(cat loan.pid) && rm loan.pid
+kill -9 $(cat loan.pid) && rm -f loan.pid
 
 echo stopping applicant service
-kill -9 $(cat applicant.pid) && rm applicant.pid
+kill -9 $(cat applicant.pid) && rm -f applicant.pid
 
 echo stopping loan model service
-kill -9 $(cat loan-model.pid) && rm loan-model.pid
+kill -9 $(cat loan-model.pid) && rm -f loan-model.pid
 
 echo stopping lra-coordinator
 podman kill --cidfile lra-coordinator.cid
